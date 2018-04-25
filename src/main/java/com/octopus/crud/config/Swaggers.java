213d44d2aa54f38112b40175ee158f6f88a63fda
yuchu2016 <1804953654@qq.com>
2018-04-25 17:31:25 +0800

    spring-data-jpa-generator生成代码后使用示例

---
+++ src/main/java/com/octopus/crud/config/Swaggers.java
@@ -0,0 +1,53 @@
+package com.octopus.crud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by luqinglin on 2017/12/1.
+ */
+@Configuration
+@EnableSwagger2
+public class Swaggers {
+
+    /**
+     * swagger配置
+     * @return
+     */
+    @Bean
+    public Docket createRestApi() {
+
+        /**
+         * swagger需要扫描的包
+         */
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.octopus.crud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    /**
+     * 界面信息
+     * @return
+     */
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多Spring Boot相关文章请关注：xxx")
+                .termsOfServiceUrl("http://xxx")
+                .contact("hahaha")
+                .version("1.0")
+                .build();
+    }
+
+
+}
